9e19a0d4f873996fc9ab5ca8e1bdd191e8c205f4
Tobias <tobiasirastorza@gmail.com>
2021-07-21 22:12:33 -0300

    mejoradas funciones chapter 11

--- 11-chapter/second-act/test_employee.py
+++ 11-chapter/second-act/test_employee.py
@@ -7,14 +7,14 @@ class TestEmployee(unittest.TestCase):
         self.employee = Employee('Tobias', 'Irastorza', 50000)
     
     def test_give_default_raise(self):
-        salario = self.employee.anual_salary + 5000 # variable salario que simula ya el aumento
+        """" Test that a default raise works correctly. """
         self.employee.give_raise() # realizo el aumento
-        self.assertEqual(salario, self.employee.anual_salary) # comparo si el aumento es igual a mi simulacion del aumento
+        self.assertEqual(self.employee.anual_salary, 55000) # comparo si el aumento es igual a mi simulacion del aumento
         
     def test_give_custom_raise(self):
-        salario = self.employee.anual_salary + 2000
+        """ Test that a custom raise works correctly """
         self.employee.anual_salary += 2000
-        self.assertEqual(salario, self.employee.anual_salary)
+        self.assertEqual(self.employee.anual_salary, 52000)
 
 
 if __name__ == '__main__':
